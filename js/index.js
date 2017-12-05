f4a575d114b70f3a1f8735c8b136095768790e0e
mo-ro <gainmoro@gmail.com>
2017-12-05 19:41:48 +0900

    opening追加

--- js/index.js
+++ js/index.js
@@ -1,87 +1,153 @@
-//トップスライドショー
-// $(".top").vegas({
-//     slides: [
-//         { src: '../img/about2.jpg' },
-//         { src: '../img/lock.jpg' },
-//         { src: '../img/top.jpg' },
-//         { src: '../img/top2.jpg' }
-//     ],
-//     animation: 'random'
-// });
+// import { setTimeout } from "timers";
+
+// トップスライドショー
+$(".top").vegas({
+  slides: [{
+      src: '../img/about2.jpg'
+    },
+    {
+      src: '../img/lock.jpg'
+    },
+    {
+      src: '../img/top.jpg'
+    },
+    {
+      src: '../img/top2.jpg'
+    }
+  ],
+  animation: 'random'
+});
 
 
-//リロード時トップに戻る
-// $(window).on('load', function(){
-// 	$('html,body').animate({ scrollTop: 0 });
-// });
+// リロード時トップに戻る
+$(window).on('load', function(){
+	$('html,body').animate({ scrollTop: 0 });
+});
+var diff = 7;
 
 
 var $svg = $('.logo').drawsvg({
-  duration: 500, 
-  stagger: 20, 
-  easing:'swing', 
-  reverse:false, 
-  callback: function() {
-    TweenMax.to(".logo", .3, {fill: "#fff"});
-    TweenMax.to(".top-filter", 2, {backgroundColor: "rgba(0, 0, 0, 0.65)", delay: .8});
-    
-    TweenMax.to(".top-overlay2", 1, {left: "100%", delay: .8, ease: Expo.easeOut});
+  duration: 500,
+  stagger: 20,
+  easing: 'swing',
+  reverse: false,
+  callback: function () {
+    TweenMax.to(".logo", .3, {
+      fill: "#fff"
+    });
+    TweenMax.to(".top-filter", 2, {
+      backgroundColor: "rgba(0, 0, 0, 0.65)",
+      delay: .8
+    });
+    TweenMax.to(".top-overlay2", 1, {
+      left: "100%",
+      delay: .8,
+      ease: Expo.easeOut
+    });
   }
 });
 
-$svg.drawsvg('animate');
+setTimeout(function() {
+}, 2000);
 
-// $('.Grobal_wrapper').onepage_scroll({
-//   sectionContainer: 'section',
-//   easing: 'ease',
-//   animationTime: 500,
-//   loop: false,
-//   responsiveFallback: 700,
-//   pagination: false,
-//   direction: "vertical"
-// });
+function opening(diff) {
+  console.log(diff + "px")
+  TweenMax.to('.opening-lists', .2, {
+    top: diff + "rem",
+    delay: .6,
+    onComplete: function() {
+      if(diff != -7) {
+        diff -= 7;
+        opening(diff);
+      }else {
+        TweenMax.to('.opening-lists', .5, {
+          opacity: 0,
+          delay: .8,
+          onComplete: function() {
+            $svg.drawsvg('animate');
 
-// $(".top-border").drawsvg('animate');
+          }
+        })
+      }
+    }
+  });
+}
+
+opening(diff);
 
 var before = $(window).scrollTop();
 var flg = false;
-$(window).scroll(function() {
-var after = $(window).scrollTop();
+var intoFlg = false;
 
-// console.log(after)
-if(before > after && $(window).scrollTop() <= $(window).height()  && !flg) {
-  flg = true;
-  var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  $(document).on(scroll_event,function(e){e.preventDefault();});
-  $("html").animate({scrollTop:0}, 700);
-  // $("main").css("top", 0);
+$(window).scroll(function () {
+  var after = $(window).scrollTop();
 
-  var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  setTimeout(function() {
-  $(document).off(scroll_event); 
-  flg = false;
-  },900)
-}
-else if(before < after && $(window).scrollTop() > 0 && $(window).scrollTop() < 300 && !flg) {
-  flg = true;
-  var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  $(document).on(scroll_event,function(e){e.preventDefault();});
-  $("html").animate({scrollTop:$(window).height()}, 700);
-  // $("main").css("top", 0);
+  // console.log(after)
+  if (before > after && $(window).scrollTop() <= $(window).height() && !flg) {
+    flg = true;
+    var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+    $(document).on(scroll_event, function (e) {
+      e.preventDefault();
+    });
+    $("html").animate({
+      scrollTop: 0
+    }, 700);
+    // $("main").css("top", 0);
 
-  var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  setTimeout(function() {
-  flg = false;  
-  $(document).off(scroll_event); 
-  },900)
-}
+    var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+    setTimeout(function () {
+      $(document).off(scroll_event);
+      flg = false;
+    }, 900)
+  } else if (before < after && $(window).scrollTop() > 0 && $(window).scrollTop() < 300 && !flg) {
+    flg = true;
+    var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+    $(document).on(scroll_event, function (e) {
+      e.preventDefault();
+    });
+    $("html").animate({
+      scrollTop: $(window).height()
+    }, 700);
+    // $("main").css("top", 0);
 
-if($("main").css("top") == "0px" && $(window).scrollTop() == 0) {
-  // $("main").css("top", "100vh");
-}
-before = after;
+    var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
+    setTimeout(function () {
+      flg = false;
+      $(document).off(scroll_event);
+    }, 900)
+  }
+  if (isScrolledIntoView($('.about')) == true && !intoFlg) {
+    intoFlg = true;
+    TweenMax.to(".about-right-filter", .8, {
+      left: 0,
+      opacity: 1,
+      ease: Power4.easeIn
+    });
+    TweenMax.to(".about-right", 0, {
+      backgroundImage: "url('../img/about2.jpg')",
+      delay: .8
+    });
+    // $(".about-right").css("background-image", "url('../img/about2.jpg')")
+    TweenMax.to(".about-dancing", 1, {
+      color: "#F57C00",
+      delay: .8
+    });
+    TweenMax.to(".about-right-filter", .3, {
+      left: "100%",
+      opacity: 1,
+      delay: 1
+    });
+  }
+
+  before = after;
 });
 
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
 
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
 
-
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
