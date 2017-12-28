16aa281444bd88a507da3231ee156f0c0f93252d
mo-ro <gainmoro@gmail.com>
2017-12-29 01:50:29 +0900

    コメント修正

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,22 +1,20 @@
-// import { setTimeout } from "timers";
-
 // トップスライドショー
-$(".top").vegas({
-  slides: [{
-      src: './img/about2.jpg'
-    },
-    {
-      src: './img/lock.jpg'
-    },
-    {
-      src: './img/top.jpg'
-    },
-    {
-      src: './img/top2.jpg'
-    }
-  ],
-  animation: 'random'
-});
+// $(".top").vegas({
+//   slides: [{
+//       src: './img/about2.jpg'
+//     },
+//     {
+//       src: './img/lock.jpg'
+//     },
+//     {
+//       src: './img/top.jpg'
+//     },
+//     {
+//       src: './img/top2.jpg'
+//     }
+//   ],
+//   animation: 'random'
+// });
 
 
 // リロード時トップに戻る
@@ -31,7 +29,7 @@ TweenMax.to('.top-scrollButton', .8, {
   yoyo: true
 })
 
-$('.top-scrollButton').click(function() {
+$('.top-scrollButton').click(function () {
   $("html").animate({
     scrollTop: $(window).height()
   }, 700);
@@ -41,51 +39,24 @@ var diff = 100;
 var newsFlg = false;
 
 
-// var $svg = $('.logo').drawsvg({
-//   duration: 200,
-//   stagger: 20,
-//   easing: 'swing',
-//   reverse: false,
-//   callback: function () {
-//     TweenMax.to(".logo", .3, {
-//       fill: "#fff"
-//     });
-//     TweenMax.to(".top-filter", 2, {
-//       backgroundColor: "rgba(0, 0, 0, 0.65)",
-//       delay: .8
-//     });
-//     TweenMax.to(".top-overlay2", 1, {
-//       left: "100%",
-//       delay: .8,
-//       ease: Expo.easeOut
-//     });
-//   }
-// });
-
-// setTimeout(function() {
-// }, 2000);
-
 function opening(diff) {
   TweenMax.to('.opening-lists', .2, {
     top: diff + "%",
     delay: .6,
-    onComplete: function() {
-      if(diff >= 0) {
+    onComplete: function () {
+      if (diff >= 0) {
         diff -= 100;
         opening(diff);
-      }else {
+      } else {
         TweenMax.to('.opening-lists', .5, {
           opacity: 0,
           delay: .8,
-          onComplete: function() {
-            // $svg.drawsvg('animate');
+          onComplete: function () {
             TweenMax.to(".top-filter", 2, {
               backgroundColor: "rgba(0, 0, 0, 0.45)",
-              // delay: .
             });
             TweenMax.to(".top-overlay2", 1, {
               left: "100%",
-              // delay: .5,
               ease: Expo.easeOut
             });
           }
@@ -109,168 +80,85 @@ $(window).scroll(function () {
   var after = $(window).scrollTop();
   var scrl = $(window).scrollTop();
 
-  // console.log(after)
-  // if (before > after && $(window).scrollTop() <= winHeight && !flg) {
-  //   flg = true;
-  //   var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  //   $(document).on(scroll_event, function (e) {
-  //     e.preventDefault();
-  //   });
-  //   $("html, body").animate({
-  //     scrollTop: 0
-  //   }, 700);
-
-  //   var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  //   setTimeout(function () {
-  //     $(document).off(scroll_event);
-  //     flg = false;
-  //   }, 900)
-  // } else if (before < after && $(window).scrollTop() > 0 && $(window).scrollTop() < 300 && !flg) {
-  //   flg = true;
-  //   var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  //   $(document).on(scroll_event, function (e) {
-  //     e.preventDefault();
-  //   });
-  //   $("html, body").animate({
-  //     scrollTop: $(window).height()
-  //   }, 700);
-  //   TweenMax.to('main', 1, {
-  //     paddingTop: '20px'
-  //   });
-
-  //   var scroll_event = 'onwheel' in document ? 'wheel' : 'onmousewheel' in document ? 'mousewheel' : 'DOMMouseScroll';
-  //   setTimeout(function () {
-  //     flg = false;
-  //     $(document).off(scroll_event);
-  //   }, 900)
-  // }
-if(winWidth > 900){
-  if(scrl > 300 && scrl < 1100) {
-    TweenMax.to('.about', .3, {
-          right: 0,
-          ease: Power0.easeNone
-    });
-  }else {
-    TweenMax.to('.about', .3, {
-      right: '96%',
-      ease: Power0.easeNone
-    });
+  if (winWidth > 900) {
+    if (scrl > 300 && scrl < 1100) {
+      TweenMax.to('.about', .3, {
+        right: 0,
+        ease: Power0.easeNone
+      });
+      TweenMax.to('.Side-navigation__circle--about', .3, {
+        backgroundColor: "#03A9F4"
+      });
+    } else {
+      TweenMax.to('.about', .3, {
+        right: '96%',
+        ease: Power0.easeNone
+      });
+      TweenMax.to('.Side-navigation__circle--about', .3, {
+        backgroundColor: "#FFD180"
+      });
+    }
+    if (scrl > 1100 && scrl < 1600) {
+      TweenMax.to('.detail', .3, {
+        right: 0,
+        ease: Power0.easeNone
+      });
+      TweenMax.to('.Side-navigation__circle--detail', .3, {
+        backgroundColor: "#03A9F4"
+      });
+    } else {
+      TweenMax.to('.detail', .3, {
+        right: '96%',
+        ease: Power0.easeNone
+      });
+      TweenMax.to('.Side-navigation__circle--detail', .3, {
+        backgroundColor: "#FFD180"
+      });
+    }
+    if (scrl > 1600 && scrl < 2100) {
+      TweenMax.to('.news', .3, {
+        right: 0,
+        ease: Power0.easeNone
+      });
+      TweenMax.to('.Side-navigation__circle--active', .3, {
+        backgroundColor: "#03A9F4"
+      });
+    } else {
+      TweenMax.to('.news', .3, {
+        right: '96%',
+        ease: Power0.easeNone
+      });
+      TweenMax.to('.Side-navigation__circle--active', .3, {
+        backgroundColor: "#FFD180"
+      });
+    }
+    if (scrl > 2100) {
+      TweenMax.to('.genre', .3, {
+        right: 0,
+        ease: Power0.easeNone
+      });
+      TweenMax.to('.Side-navigation__circle--genre', .3, {
+        backgroundColor: "#03A9F4"
+      });
+    } else {
+      TweenMax.to('.genre', .3, {
+        right: '96%',
+        ease: Power0.easeNone
+      });
+      TweenMax.to('.Side-navigation__circle--genre', .3, {
+        backgroundColor: "#FFD180"
+      });
+    }
   }
-  if(scrl > 1100 && scrl < 1600) {
-    TweenMax.to('.detail', .3, {
-      right: 0,
-      ease: Power0.easeNone
-});
-  }else {
-    TweenMax.to('.detail', .3, {
-      right: '96%',
-      ease: Power0.easeNone
-});
-  }
-  if(scrl > 1600 && scrl < 2100) {
-    TweenMax.to('.news', .3, {
-      right: 0,
-      ease: Power0.easeNone
-});
-  }else {
-    TweenMax.to('.news', .3, {
-      right: '96%',
-      ease: Power0.easeNone
-});
-  }
-  if(scrl > 2100) {
-    TweenMax.to('.genre', .3, {
-      right: 0,
-      ease: Power0.easeNone
-});
-  }else {
-    TweenMax.to('.genre', .3, {
-      right: '96%',
-      ease: Power0.easeNone
-});
-  }
-}
 
-  // if (isScrolledIntoView($('.about')) && !intoFlg) {
-  //   TweenMax.to('.about', .8, {
-  //     right: 0,
-  //     ease: Power0.easeNone
-  //   })
-  //   intoFlg = true;
-  //   TweenMax.to('.about-left', 1, {
-  //     opacity: 1,
-  //     // delay: .3,
-  //     transform: 'translate(0, 0)'
-  //   });
-  //   TweenMax.to(".about-right-filter", .8, {
-  //     left: 0,
-  //     opacity: 1,
-  //     ease: Power4.easeIn
-  //   });
-  //   TweenMax.to(".about-right", 0, {
-  //     backgroundImage: "url('./img/about2.jpg')",
-  //     delay: .8
-  //   });
-  //   TweenMax.to(".about-right2", 0, {
-  //     backgroundImage: "url('./img/about6.jpg')",
-  //     delay: .8
-  //   });
-  //   // $(".about-right").css("background-image", "url('../img/about2.jpg')")
-  //   TweenMax.to(".about-dancing", 1, {
-  //     color: "#F57C00",
-  //     delay: .8
-  //   });
-  //   TweenMax.to(".about-right-filter", .3, {
-  //     left: "100%",
-  //     opacity: 1,
-  //     delay: 1
-  //   });
-  // }
-  // if(isScrolledIntoView($('.detail')) && !detailFlg) {
-  //   detailFlg = true;
-  //   TweenMax.to(".detail-filter", .8, {
-  //     left: 0,
-  //     opacity: 1,
-  //     ease: Power4.easeIn
-  //   });
-  //   TweenMax.to(".detail-filter", .3, {
-  //     left: "100%",
-  //     opacity: 1,
-  //     delay: 1
-  //   });
-  //   TweenMax.to(".detail-img", 0, {
-  //     display: 'block',
-  //     delay: .8
-  //   })
-  // }
-
-  // if(isScrolledIntoView($('.news')) && !newsFlg) {
-  //   newsFlg = true;
-  //   console.log(isScrolledIntoView($('.news')))
-  //   TweenMax.to('.news-list:nth-child(1)', .8, {
-  //     opacity: 1,
-  //     transform: 'translate(0, 0)'
-  //   });
-  //   TweenMax.to('.news-list:nth-child(2)', .8, {
-  //     opacity: 1,
-  //     transform: 'translate(0, 0)',
-  //     delay: .2
-  //   });
-  //   TweenMax.to('.news-list:nth-child(3)', .8, {
-  //     opacity: 1,
-  //     transform: 'translate(0, 0)',
-  //     delay: .4
-  //   });
-  // }
-
-  if(scrl >= winHeight) {
+  if (scrl >= winHeight) {
     TweenMax.to('.Side-navigation', .3, {
       left: '95%'
     });
     TweenMax.to('.logo', 0, {
       position: 'fixed'
     })
-  }else {
+  } else {
     TweenMax.to('.Side-navigation', .3, {
       left: '100%'
     })
@@ -283,11 +171,13 @@ if(winWidth > 900){
 });
 
 //サイドメニュークリックイベント
-$('a[href^="."]').click(function() {
-  var href= $(this).attr("href");
+$('a[href^="."]').click(function () {
+  var href = $(this).attr("href");
   var target = $(href == "" ? 'html' : href);
   var position = target.offset().top;
-  $('body,html').animate({scrollTop:position+1}, 450, 'swing');
+  $('body,html').animate({
+    scrollTop: position + 1
+  }, 450, 'swing');
   return false;
 });
 
@@ -297,7 +187,6 @@ function isScrolledIntoView(elem) {
 
   var elemTop = $(elem).offset().top;
   var elemBottom = elemTop + $(elem).height();
-  // console.log((elemBottom <= docViewBottom) && (elemTop >= docViewTop))
 
   return ((elemTop >= docViewTop) && (elemBottom <= docViewBottom));
 }
